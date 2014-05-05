2a5b8f3748d19b0a19efd32e193c549723108f02
Harvey Xia <harvey@harveyxia.com>
2014-05-05 18:57:49 -0400

    Add login

--- test/app.js
+++ test/app.js
@@ -1,8 +1,29 @@
+var dataRef = new Firebase('https://popping-fire-7822.firebaseio.com');
 var myApp = angular.module('myApp', ['firebase', ]);
 
-myApp.controller('myController', ['$scope', '$firebase',
+// SimpleLogin
+myApp.controller('loginController', ['$scope', '$firebase', '$firebaseSimpleLogin',
+  function($scope, $firebase, $firebaseSimpleLogin) {
+
+    $scope.auth = $firebaseSimpleLogin(dataRef);
+
+    $scope.login = function() {
+      $scope.auth.$login('password', {
+          email: $scope.loginEmail,
+          password: $scope.loginPassword
+        }).then(function(user) {
+          $scope.loginMessage = 'Logged in as: ' + user.uid;
+        }, function(error) {
+          $scope.loginMessage = 'Login failed: ' + error;
+        });
+    };
+  }
+]);
+
+// retrieves quotes
+myApp.controller('quoteController', ['$scope', '$firebase',
   function($scope, $firebase) {
-    var ref = new Firebase("https://popping-fire-7822.firebaseio.com/users/simplelogin%3A2/quotes");
-    $scope.quotes = $firebase(ref);
+    var userRef = new Firebase('https://popping-fire-7822.firebaseio.com/users/simplelogin%3A2/quotes');
+    $scope.quotes = $firebase(userRef);
   }
 ]);
