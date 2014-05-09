1de8ce6bbf2a4af2f88975573f66ee4c0b5d20ef
Harvey Xia <harvey@harveyxia.com>
2014-05-09 11:44:53 -0400

    fixes to auto expanding textarea

--- test/js/utility.js
+++ test/js/utility.js
@@ -1,10 +1,8 @@
 $(document).ready(function () {
-    console.log('sdfasdf')
-    // auto adjust the height of
+    // auto expand text area
     $('.new-quote').on( 'keyup', 'textarea', function (e){
         $(this).css('height', 'auto' );
         $(this).height( this.scrollHeight );
-        console.log(this.scrollHeight);
     });
     // $('.new-quote').find( 'textarea' ).keyup(function() {
     //     });
